d4b87e0b1280e8216ffd5fbc9060410124172255
Phu Ngo <ptn123@gmail.com>
2021-06-09 16:30:55 -0500

    images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import Hello from "./components/Hello.jsx"
+import Flat from "./components/Flat.jsx"
 
 //the return here is JSX but it looks like HTML. Babel will convert this to Javascript and JSX
+
+const tempFlat =  {
+  "id": 145,
+  "name": "The Stay Chez Claudette",
+  "imageUrl": "https://raw.githubusercontent.com/phutngo/airbnb-clone/master/images/chez-claudette-tripadvisor.jpg",
+  "price": 164,
+  "priceCurrency": "CAD",
+  "lat": 45.5264082,
+  "lng": -73.5900536
+};
+
 
 function App() {
   return (
     <div>
+    
+    <Flat data = {tempFlat} />
 
     </div>
   )
--- src/components/Flat.jsx
+++ src/components/Flat.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
-//https://raw.githubusercontent.com/clairefro/flats-boilerplate/master/flats.json
-//The raw.githubusercontent.com domain is used to serve unprocessed versions of files stored in GitHub repositories. If you browse to a file on GitHub and then click the Raw link,
+/* https://raw.githubusercontent.com/phutngo/airbnb-clone/main/flats.json
+
+The raw.githubusercontent.com domain is used to serve unprocessed versions of files stored in GitHub repositories. If you browse to a file on GitHub and then click the Raw link you get this link. 
+
+We will use this link as if it was an API endpoint to practice fetching data from it.
+*/
 
 
-const Flat = (props) => {
+const Flat = ( {data} ) => {
     return (
     <>
-    <div className="flat"></div>
-    <div className="flat-pic"></div>
-    <div className="flat-title"></div>
+    <div className="flat">
+      <div 
+        className="flat-picture" 
+        style={ {backgroundImage: `url('${data.imageUrl}')`}} 
+      ></div>
+      <div className="flat-title"> 
+        {data.name} - {data.price} {data.priceCurrency}
+      </div>
+    </div>
     </>    
     );
 };
